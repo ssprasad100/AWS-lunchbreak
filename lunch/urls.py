08630d1f4a6b72657b02ca842c5c757076e5fe7f
Andreas Backx <andreas@backx.org>
2014-10-19 15:21:53 +0200

    Link veranderd voor zaken dichtbij te krijgen.

--- lunch/urls.py
+++ lunch/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from lunch import views
 
 urlpatterns = patterns('',
-    url(r'stores/(?P<latitude>\d+)/(?P<longitude>\d+)/(?P<proximity>\d+)/$', views.StoreListView.as_view()),
-    url(r'stores/(?P<latitude>.+)/(?P<longitude>.+)/$', views.StoreListView.as_view()),
+    url(r'stores/nearby/(?P<latitude>\d+)/(?P<longitude>\d+)/(?P<proximity>\d+)/$', views.StoreListView.as_view()),
+    url(r'stores/nearby/(?P<latitude>.+)/(?P<longitude>.+)/$', views.StoreListView.as_view()),
     url(r'stores/(?P<id>.+)/$', views.StoreListView.as_view()),
 
     url(r'food/store/(?P<store_id>.+)/$', views.FoodListView.as_view()),
