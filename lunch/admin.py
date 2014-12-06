f2292630b5d18a8b06b837238b2f802874881f0d
Andreas Backx <andreas@backx.org>
2014-12-06 23:37:58 +0100

    Added all models to the admin interface.

--- lunch/admin.py
+++ lunch/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from lunch.models import Store, StoreCategory, DefaultFood, Food, DefaultIngredient, Ingredient, IngredientGroup
+from lunch.models import Store, StoreCategory, DefaultFood, Food, DefaultIngredient, Ingredient, IngredientGroup, Token, User
+
+
+admin.site.register(StoreCategory)
 
 
 @admin.register(Store)
@@ -7,20 +10,44 @@ class StoreAdmin(admin.ModelAdmin):
 	readonly_fields = ('latitude', 'longitude',)
 
 
-admin.site.register(StoreCategory)
-
-
 @admin.register(DefaultFood)
 class DefaultFoodAdmin(admin.ModelAdmin):
+	list_display = ('name', 'cost',)
 	fields = ('name', 'cost', 'ingredients',)
 	filter_horizontal = ('ingredients',)
 
 
 @admin.register(Food)
 class FoodAdmin(DefaultFoodAdmin):
+	list_display = ('name', 'cost', 'store',)
 	fields = ('name', 'cost', 'ingredients', 'store',)
 
-admin.site.register(DefaultIngredient)
-admin.site.register(Ingredient)
 
-admin.site.register(IngredientGroup)
+@admin.register(DefaultIngredient)
+class DefaultIngredientAdmin(admin.ModelAdmin):
+	list_display = ('name', 'cost', 'group',)
+	fields = ('name', 'cost', 'group',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(DefaultIngredientAdmin):
+	list_display = ('name', 'cost', 'group', 'store')
+	fields = ('name', 'cost', 'group', 'store')
+
+
+@admin.register(IngredientGroup)
+class IngredientGroupAdmin(admin.ModelAdmin):
+	list_display = ('name', 'maximum',)
+	fields = ('name', 'maximum',)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('phone', 'name', 'userId', 'requestId', 'confirmed', 'confirmedAt',)
+	fields = ('phone', 'name', 'userId', 'requestId', 'confirmed', 'confirmedAt',)
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+	list_display = ('identifier', 'device', 'user',)
+	fields = ('identifier', 'device', 'user',)
