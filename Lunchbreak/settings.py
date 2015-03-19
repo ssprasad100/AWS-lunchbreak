295ddbd67e07eff7787e8c7e1d03c93506590bcd
Andreas Backx <andreas@backx.org>
2015-03-19 14:24:47 +0100

    Pagination added, see docs for information.
    Update #57

--- Lunchbreak/settings.py
+++ Lunchbreak/settings.py
@@ -83,7 +83,9 @@ class Base(Configuration):
         ],
         'DEFAULT_PARSER_CLASSES': (
             'rest_framework.parsers.JSONParser',
-        )
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 25
     }
 
     OPBEAT = {
--- docs/other.rst
+++ docs/other.rst
@@ -11,3 +11,10 @@ DateTime
 
 Time
     * ```%H:%M```
+
+
+Pagination
+==========
+
+Examples `here <http://www.django-rest-framework.org/api-guide/pagination/#pagenumberpagination>`_
+.
