225e131c28ca93d47ae2d4d55ca55522128d965c
Andreas Backx <andreas@backx.org>
2015-04-04 16:21:27 +0200

    StoreCategory list request added for both business and customers.
    * Reorganised url patterns a bit.
    * Added icon field to StoreCategorySerializer.

--- business/urls.py
+++ business/urls.py
@@ -2,35 +2,37 @@ from django.conf.urls import patterns, url
 from business import views
 
 urlpatterns = patterns('',
-    url(r'employee/$', views.EmployeeView.as_view()),
-    url(r'employee/(?P<id>\d+)/$', views.EmployeeView.as_view()),
-    url(r'employee/reset/request/(?P<employee_id>\d+)/$', views.EmployeeRequestResetView.as_view()),
+    url(r'^employee/$', views.EmployeeView.as_view()),
+    url(r'^employee/(?P<id>\d+)/$', views.EmployeeView.as_view()),
+    url(r'^employee/reset/request/(?P<employee_id>\d+)/$', views.EmployeeRequestResetView.as_view()),
 
-    url(r'food/$', views.FoodListView.as_view()),
-    url(r'food/(?P<pk>\d+)/$', views.FoodRetrieveView.as_view()),
-    url(r'food/default/$', views.DefaultFoodListView.as_view()),
-    url(r'food/default/(?P<pk>\d+)/$', views.DefaultFoodRetrieveView.as_view()),
+    url(r'^food/$', views.FoodListView.as_view()),
+    url(r'^food/(?P<pk>\d+)/$', views.FoodRetrieveView.as_view()),
+    url(r'^food/default/$', views.DefaultFoodListView.as_view()),
+    url(r'^food/default/(?P<pk>\d+)/$', views.DefaultFoodRetrieveView.as_view()),
 
-    url(r'ingredient/$', views.IngredientListView.as_view()),
-    url(r'ingredient/default/$', views.DefaultIngredientListView.as_view()),
+    url(r'^foodcategory/$', views.FoodCategoryListView.as_view()),
 
-    url(r'foodcategory/$', views.FoodCategoryListView.as_view()),
+    url(r'^foodtype/$', views.FoodTypeListView.as_view()),
 
-    url(r'foodtype/$', views.FoodTypeListView.as_view()),
+    url(r'^ingredient/$', views.IngredientListView.as_view()),
+    url(r'^ingredient/default/$', views.DefaultIngredientListView.as_view()),
 
-    url(r'ingredientgroup/$', views.IngredientGroupListView.as_view()),
+    url(r'^ingredientgroup/$', views.IngredientGroupListView.as_view()),
 
-    url(r'order/(?P<option>pickupTime|orderedTime)?/?$', views.OrderListView.as_view()),
-    url(r'order/(?P<pk>\d+)/$', views.OrderUpdateView.as_view()),
+    url(r'^order/(?P<option>pickupTime|orderedTime)?/?$', views.OrderListView.as_view()),
+    url(r'^order/(?P<pk>\d+)/$', views.OrderUpdateView.as_view()),
 
-    url(r'staff/$', views.StaffView.as_view()),
-    url(r'staff/(?P<id>\d+)/$', views.StaffView.as_view()),
-    url(r'staff/nearby/(?P<latitude>\d+.?\d*)/(?P<longitude>\d+.?\d*)/$', views.StaffView.as_view()),
-    url(r'staff/nearby/(?P<latitude>\d+.?\d*)/(?P<longitude>\d+.?\d*)/(?P<proximity>\d+.?\d*)/$', views.StaffView.as_view()),
-    url(r'staff/reset/request/(?P<email>.+)/$', views.StaffRequestResetView.as_view()),
-    url(r'staff/reset/(?P<email>.+)/(?P<passwordReset>.+)/$', views.StaffResetView.as_view()),
+    url(r'^staff/$', views.StaffView.as_view()),
+    url(r'^staff/(?P<id>\d+)/$', views.StaffView.as_view()),
+    url(r'^staff/nearby/(?P<latitude>\d+.?\d*)/(?P<longitude>\d+.?\d*)/$', views.StaffView.as_view()),
+    url(r'^staff/nearby/(?P<latitude>\d+.?\d*)/(?P<longitude>\d+.?\d*)/(?P<proximity>\d+.?\d*)/$', views.StaffView.as_view()),
+    url(r'^staff/reset/request/(?P<email>.+)/$', views.StaffRequestResetView.as_view()),
+    url(r'^staff/reset/(?P<email>.+)/(?P<passwordReset>.+)/$', views.StaffResetView.as_view()),
 
     url(r'^store/open/$', views.StoreOpenView.as_view()),
     url(r'^store/hours/$', views.OpeningHoursListView.as_view()),
     url(r'^store/holiday/$', views.HolidayPeriodListView.as_view()),
+
+    url(r'^storecategory/$', views.StoreCategoryListView.as_view()),
 )
