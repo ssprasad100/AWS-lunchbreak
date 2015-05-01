e0985465af76e106babaa03e7bff12c514995e67
Andreas Backx <andreas@backx.org>
2015-05-01 19:06:40 +0200

    WrongAPIVersionView only for older APIs.

--- Lunchbreak/urls.py
+++ Lunchbreak/urls.py
@@ -5,5 +5,5 @@ from lunch import views
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^v1/', include('lunch.urls')),
-    url(r'^v(0|[2-9]\d*)|(1\d+)/', views.WrongAPIVersionView.as_view())
+    url(r'^v0/', views.WrongAPIVersionView.as_view())
 )
