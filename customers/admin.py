2c1bf0bd17dd795b41273a84ee9a0e088745e592
Andreas Backx <andreas@backx.org>
2015-06-29 13:17:28 +0200

    All Digits error codes added.

--- customers/admin.py
+++ customers/admin.py
@@ -19,7 +19,7 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('user', 'store', 'orderedTime', 'pickupTime', 'status', 'paid',)
-    fields = ('user', 'store', 'pickupTime', 'status', 'paid', 'orderedTime', 'description',)
+    fields = ('user', 'store', 'pickupTime', 'status', 'paid', 'total', 'orderedTime', 'description',)
     readonly_fields = ('orderedTime',)
 
 
