b455db32b7c59419f5948f74910895a364571ab8
Andreas Backx <andreas@backx.org>
2015-08-22 19:37:24 +0200

    Added version 3 as allowed and v2 header compat.

--- lunch/authentication.py
+++ lunch/authentication.py
@@ -6,13 +6,13 @@ class TokenAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         identifier = request.META.get('HTTP_X_IDENTIFIER')
         modelId = request.META.get('HTTP_X_' + self.MODEL_NAME.upper())
-        device = request.META.get('HTTP_X_DEVICE')
+        device = request.META.get('HTTP_X_NAME') if request.version > 2 else request.META.get('HTTP_X_DEVICE')
 
         if not identifier or not modelId or not device:
             raise AuthenticationFailed('Not all of the headers were provided.')
 
         try:
-            arguments = {self.MODEL_NAME + '_id': modelId, 'identifier': identifier, 'device': device}
+            arguments = {self.MODEL_NAME + '_id': modelId, 'identifier': identifier, 'name': device}
             modelToken = self.TOKEN_MODEL.objects.get(**arguments)
         except:
             raise AuthenticationFailed('%sToken not found.' % self.MODEL_NAME.capitalize())
--- lunch/versioning.py
+++ lunch/versioning.py
@@ -3,7 +3,7 @@ from rest_framework.versioning import BaseVersioning
 
 class HeaderVersioning(BaseVersioning):
     default_version = 1
-    allowed_versions = [1, 2]
+    allowed_versions = [1, 2, 3]
 
     def determine_version(self, request, *args, **kwargs):
         return int(request.META.get('HTTP_X_VERSION', HeaderVersioning.default_version))
