5118cce224412acf7b7fd4c3ea32c31d4e999413
Andreas Backx <andreas@backx.org>
2016-02-23 18:09:22 +0100

    Removed lunch.urls.

--- lunchbreak/Lunchbreak/urls.py
+++ lunchbreak/Lunchbreak/urls.py
@@ -3,8 +3,26 @@ from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^aardappel/', include(admin.site.urls)),
-    url(r'', include('lunch.urls')),
-    url(r'^', include('private_media.urls')),
+urlpatterns = patterns(
+    '',
+    url(
+        r'^aardappel/',
+        include(admin.site.urls)
+    ),
+    url(
+        r'^business/',
+        include('business.urls')
+    ),
+    url(
+        r'^customers/',
+        include('customers.urls')
+    ),
+    url(
+        r'^',
+        include('frontend.urls')
+    ),
+    url(
+        r'^',
+        include('private_media.urls')
+    ),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
