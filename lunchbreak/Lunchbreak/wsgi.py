cba7798281522c521724a1ead52191af04979b1a
Andreas Backx <andreas@backx.org>
2016-02-23 18:07:30 +0100

    Uwsgi try and catch for development server.

--- lunchbreak/Lunchbreak/wsgi.py
+++ lunchbreak/Lunchbreak/wsgi.py
@@ -1,17 +1,20 @@
 import os
 
-import uwsgi
 from django.core.wsgi import get_wsgi_application
 from django.utils import autoreload
-from uwsgidecorators import timer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lunchbreak.settings')
 os.environ.setdefault('DJANGO_SETTINGS_BRANCH', 'development')
 
 application = get_wsgi_application()
 
+try:
+    import uwsgi
+    from uwsgidecorators import timer
 
-@timer(3)
-def change_code_graceful_reload(sig):
-    if autoreload.code_changed():
-        uwsgi.reload()
+    @timer(3)
+    def change_code_graceful_reload(sig):
+        if autoreload.code_changed():
+            uwsgi.reload()
+except ImportError:
+    pass
