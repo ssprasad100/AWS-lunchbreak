b0c6bd42a5b4575d99283d53a396ce256c8e3179
Andreas Backx <andreas@backx.org>
2016-10-23 22:17:39 +0200

    Ignore docker image deletion errors.

--- fabfile.py
+++ fabfile.py
@@ -330,7 +330,8 @@ class Deployer:
                 )
 
         # Delete all dangling images
-        run('docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi')
+        with settings(warn_only=True):
+            run('docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi')
 
     def register_opbeat(self):
         with hide('running'):
