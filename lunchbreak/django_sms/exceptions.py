1854cd881b3ed6b455c1baabd99a16c9de5f92b9
Andreas Backx <andreas@backx.org>
2016-11-02 23:07:48 +0100

    Updated django_sms.exceptions for the exception handler.

--- lunchbreak/django_sms/exceptions.py
+++ lunchbreak/django_sms/exceptions.py
@@ -9,27 +9,27 @@ PIN_TIMEOUT = 299
 
 class SmsException(LunchbreakException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    code = UNKNOWN_ERROR
-    information = 'Unknown error.'
+    default_code = UNKNOWN_ERROR
+    default_detail = 'Unknown error.'
 
 
 class PinExpired(SmsException):
     status_code = status.HTTP_400_BAD_REQUEST
-    code = PIN_EXPIRED
-    information = 'Given pin code has expired.'
+    default_code = PIN_EXPIRED
+    default_detail = 'Given pin code has expired.'
 
 
 class PinTriesExceeded(PinExpired):
-    information = 'Max amount of pin tries exceeded.'
+    default_detail = 'Max amount of pin tries exceeded.'
 
 
 class PinIncorrect(SmsException):
     status_code = status.HTTP_400_BAD_REQUEST
-    code = PIN_INCORRECT
-    information = 'Given pin code is incorrect.'
+    default_code = PIN_INCORRECT
+    default_detail = 'Given pin code is incorrect.'
 
 
 class PinTimeout(SmsException):
     status_code = status.HTTP_400_BAD_REQUEST
-    code = PIN_TIMEOUT
-    information = 'Given pin code is incorrect.'
+    default_code = PIN_TIMEOUT
+    default_detail = 'Given pin code is incorrect.'
