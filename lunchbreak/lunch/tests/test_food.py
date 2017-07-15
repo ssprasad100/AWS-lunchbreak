9bf3cf9eedd56c95605c094cd11b24a233a0479e
Andreas Backx <andreas@backx.org>
2017-07-15 03:57:05 +0200

    Added tests for preorder settings.

--- lunchbreak/lunch/tests/test_food.py
+++ lunchbreak/lunch/tests/test_food.py
@@ -45,81 +45,123 @@ class FoodTestCase(LunchbreakTestCase):
             preorder_time=preorder_time
         )
 
-        now = datetime.now()
-        now = now.replace(hour=12, minute=0)
+        def do_tests(now, pickup, preorder_disabled):
+            for i in range(2):
+                # Ordering without preorder_days should always return true
+                now -= timedelta(hours=1)
+                self.assertTrue(food.is_orderable(pickup, now=now))
 
-        pickup = datetime.now()
-        pickup = pickup.replace(hour=12, minute=0)
+                now += timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
 
-        for i in range(2):
-            # Ordering without preorder_days should always return true
-            now -= timedelta(hours=1)
-            self.assertTrue(food.is_orderable(pickup, now=now))
+                # Ordering before the preorder_time should add 1 preorder_day in the background
+                # and should therefore return false if wanting to pick up the next day.
 
-            now += timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
+                # with preorder_days == 0, same day order is possible
+                food.preorder_days = 0
+                food.save()
+                now -= timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
 
-            # Ordering before the preorder_time should add 1 preorder_day in the background
-            # and should therefore return false if wanting to pick up the next day.
+                now += timedelta(hours=2)
+                self.assertEqual(
+                    food.is_orderable(pickup, now=now),
+                    preorder_disabled
+                )
 
-            # with preorder_days == 0, same day order is possible
-            food.preorder_days = 0
+                # with preorder_days == 1, same day order is impossible
+                food.preorder_days = 1
+                food.save()
+                now -= timedelta(hours=2)
+                self.assertEqual(
+                    food.is_orderable(pickup, now=now),
+                    preorder_disabled
+                )
+
+                now += timedelta(hours=2)
+                self.assertEqual(
+                    food.is_orderable(pickup, now=now),
+                    preorder_disabled
+                )
+
+                # with preorder_days == 1:
+                #   * ordering before preorder_time should allow for next day ordering.
+                #   * ordering after preorder_time should not allow for next day ordering.
+                pickup += timedelta(days=1)
+                now -= timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                now += timedelta(hours=2)
+                self.assertEqual(
+                    food.is_orderable(pickup, now=now),
+                    preorder_disabled
+                )
+
+                # If it's ordered for within 2 days, before/after preorder_time doesn't matter
+                pickup += timedelta(days=1)
+                now -= timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                now += timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                # with preorder_days == 2:
+                #   * ordering before preorder_time should allow for 2 day ordering.
+                #   * ordering after preorder_time should not allow for 2 day ordering.
+                food.preorder_days = 2
+                food.save()
+                now -= timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                now += timedelta(hours=2)
+                self.assertEqual(
+                    food.is_orderable(pickup, now=now),
+                    preorder_disabled
+                )
+
+                # If it's ordered for within 3 days, before/after preorder_time doesn't matter
+                pickup += timedelta(days=1)
+                now -= timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                now += timedelta(hours=2)
+                self.assertTrue(food.is_orderable(pickup, now=now))
+
+                food.preorder_days = None
+                food.save()
+                now = datetime.now()
+                now = now.replace(hour=12, minute=0)
+                pickup = datetime.now()
+                pickup = pickup.replace(hour=14, minute=0)
+
+        for j in range(2):
+            food.preorder_days = None
             food.save()
-            now -= timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
 
-            now += timedelta(hours=2)
-            self.assertFalse(food.is_orderable(pickup, now=now))
-
-            # with preorder_days == 1, same day order is impossible
-            food.preorder_days = 1
-            food.save()
-            now -= timedelta(hours=2)
-            self.assertFalse(food.is_orderable(pickup, now=now))
-
-            now += timedelta(hours=2)
-            self.assertFalse(food.is_orderable(pickup, now=now))
-
-            # with preorder_days == 1:
-            #   * ordering before preorder_time should allow for next day ordering.
-            #   * ordering after preorder_time should not allow for next day ordering.
-            pickup += timedelta(days=1)
-            now -= timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            now += timedelta(hours=2)
-            self.assertFalse(food.is_orderable(pickup, now=now))
-
-            # If it's ordered for within 2 days, before/after preorder_time doesn't matter
-            pickup += timedelta(days=1)
-            now -= timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            now += timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            # with preorder_days == 2:
-            #   * ordering before preorder_time should allow for 2 day ordering.
-            #   * ordering after preorder_time should not allow for 2 day ordering.
-            food.preorder_days = 2
-            food.save()
-            now -= timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            now += timedelta(hours=2)
-            self.assertFalse(food.is_orderable(pickup, now=now))
-
-            # If it's ordered for within 3 days, before/after preorder_time doesn't matter
-            pickup += timedelta(days=1)
-            now -= timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            now += timedelta(hours=2)
-            self.assertTrue(food.is_orderable(pickup, now=now))
-
-            food.preorder_days = -1
-            food.save()
             now = datetime.now()
             now = now.replace(hour=12, minute=0)
+
             pickup = datetime.now()
-            pickup = pickup.replace(hour=14, minute=0)
+            pickup = pickup.replace(hour=12, minute=0)
+
+            do_tests(
+                now=now,
+                pickup=pickup,
+                preorder_disabled=food.preorder_disabled
+            )
+
+            foodtype.preorder_days = food.preorder_days
+            foodtype.preorder_time = food.preorder_time
+            foodtype.save()
+
+            food.preorder_days = None
+            food.save()
+
+        food.preorder_disabled = True
+        food.save()
+
+        do_tests(
+            now=now,
+            pickup=pickup,
+            preorder_disabled=food.preorder_disabled
+        )
