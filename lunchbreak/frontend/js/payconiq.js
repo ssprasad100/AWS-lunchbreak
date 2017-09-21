3acc5134f89fc119b9d836a4f3209cc9afe30252
Andreas Backx <andreas@backx.org>
2017-09-21 19:30:47 +0200

    Added timeouts to Payconiq web app.

--- lunchbreak/frontend/js/payconiq.js
+++ lunchbreak/frontend/js/payconiq.js
@@ -1,6 +1,12 @@
 (function() {
     'use strict';
 
+    var timeout = 5000;
+
+    var redirect = function(url) {
+        window.location.href = url;
+    };
+
     $(document).ready(function() {
         var info = $('#payconiq-info')
         payconiq({
@@ -15,19 +21,34 @@
             }
         }).on('success', function() {
             info.text('Betaling succesvol verlopen, je wordt doorverwezen naar de bevestigingspagina...');
-            window.location.href = successUrl;
+
+            setTimeout(function() {
+                redirect(successUrl);
+            }, 0);
         }).on('failed', function() {
             info.text('Betaling gefaald, je wordt terug gestuurd naar naar de vorige pagina...');
-            window.location.href = errorUrl;
+
+            setTimeout(function() {
+                redirect(errorUrl);
+            }, timeout);
         }).on('canceled', function() {
             info.text('Betaling geannuleerd, je wordt terug gestuurd naar naar de vorige pagina...');
-            window.location.href = errorUrl;
+
+            setTimeout(function() {
+                redirect(errorUrl);
+            }, timeout);
         }).on('close', function() {
             info.text('Betaling geannuleerd, je wordt terug gestuurd naar naar de vorige pagina...');
-            window.location.href = errorUrl;
+
+            setTimeout(function() {
+                redirect(errorUrl);
+            }, timeout);
         }).on('error', function() {
             info.text('Er is een fout opgetreden tijdens de betaling, je wordt terug gestuurd naar naar de vorige pagina...');
-            window.location.href = errorUrl;
+
+            setTimeout(function() {
+                redirect(errorUrl);
+            }, timeout);
         }).load();
     });
 })();
